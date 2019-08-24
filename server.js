9c2a38bf0495d5afb4f19928b3d383bce200f944
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-08-24 12:25:28 -0400

    adding heroku

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoScrapper", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/mongoScrapper", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
